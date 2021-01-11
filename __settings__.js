955020188c504678d16ce37f79743332ae268722
Andrew Zhao <andrewhzhao@gmail.com>
2021-01-11 16:11:09 -0500

    raw build

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1062012.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 40503371, 40503403, 40503372, 40503415, 40503417, 40512569, 40516931, 40523677, 40616706 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/40503363/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/40503361/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/40503362/1/ammo.js', 'preload' : true},
+];
